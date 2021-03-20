d1cda276fa3b4a0efab588af08c8e0a862c1b2da
DanyiZ <zoltan.danyi@gmail.com>
2021-03-20 14:07:38 +0100

    Basic Rest App with Hello end point

---
+++ src/main/java/hu/restapp/controller/HelloController.java
@@ -0,0 +1,13 @@
+package hu.restapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World RESTful with Spring Boot";
+    }
+}
