73fb5d6e12764b0a581cbdbabedce6b7dab8357e
danyiz <zoltan.danyi@gmail.com>
2021-09-21 15:50:17 +0200

    Remove unused imports from SpringRestApplication

--- src/main/java/hu/restapp/SpringRestApplication.java
+++ src/main/java/hu/restapp/SpringRestApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
-
 @Slf4j
 @SpringBootApplication
 public class SpringRestApplication {
@@ -25,15 +23,6 @@ public class SpringRestApplication {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-
-//           log.info("Let's inspect the beans provided by Spring Boot:");
-//
-//            String[] beanNames = ctx.getBeanDefinitionNames();
-//            Arrays.sort(beanNames);
-//            for (String beanName : beanNames) {
-//                log.info(beanName);
-//            }
-
         };
     }
 }
