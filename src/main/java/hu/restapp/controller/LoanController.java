5d53fd4e8bf9aa6f8468989940ec3833abe81903
danyi <zoltan.danyi@gmail.com>
2021-11-23 18:27:15 +0100

    Change calcloan to post mappping

--- src/main/java/hu/restapp/controller/LoanController.java
+++ src/main/java/hu/restapp/controller/LoanController.java
@@ -7,6 +7,9 @@ import hu.restapp.systemutility.SystemFrequency;
 import hu.restapp.systemutility.TriggerDateUtility;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -14,6 +17,9 @@ import java.util.List;
 
 @RestController
 @Slf4j
+@RequestMapping(path = "/",
+                consumes = MediaType.APPLICATION_JSON_VALUE,
+                produces = MediaType.APPLICATION_JSON_VALUE)
 public class LoanController {
 
     @Autowired
@@ -24,28 +30,23 @@ public class LoanController {
 
     @Autowired
     RetailLoanCalculator retailLoanCalculator;
-    @GetMapping("/getsystemdate")
-    public String getSystemDate() {
-        log.info("Sysdate is called");
-        return new TriggerDateUtility().getSystemDate().toString();
-    }
 
-     @GetMapping("/getnextdate")
+    @GetMapping("/getnextdate")
     public String getNextDate() {
         log.info("Next date is called");
         LocalDate localDate = LocalDate.now();
         return new TriggerDateUtility().calculateNextDateFromDate(localDate, SystemFrequency.YEARLY,18).toString();
     }
 
-     @GetMapping("/calcdays")
+    @GetMapping("/calcdays")
     public String calcDays() {
         log.info("CalcDate is called");
         return new TriggerDateUtility().calcDaysBetweenDates(LocalDate.now(),LocalDate.now().minusDays(200)).toString();
     }
 
-     @GetMapping("/calcloan")
-     public List<RetailLoanScheduleDTO> calcLoan(@RequestParam String loanAttributes) throws IOException {
-        RetailLoanAttributes retailLoanAttributes = objectMapper.readValue(loanAttributes,RetailLoanAttributes.class);
+     @PostMapping("/calcloan")
+     public ResponseEntity<List<RetailLoanScheduleDTO>> calcLoan(@RequestBody RetailLoanAttributes retailLoanAttributes) throws IOException {
+
         log.info("CalcLoan is called");
         try {
              Currency currency = currencyDAO.getCurrency(retailLoanAttributes.getCurrencyCode());
@@ -54,6 +55,6 @@ public class LoanController {
              e.printStackTrace();
         }
 
-        return retailLoanCalculator.generateSchedule(true,retailLoanAttributes);
+        return new ResponseEntity<List<RetailLoanScheduleDTO>>(retailLoanCalculator.generateSchedule(true,retailLoanAttributes), HttpStatus.OK);
     }
 }
